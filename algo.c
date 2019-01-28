dcf51a0303e30def5054ab9f463de5fc9711289a
Paul RASTOIN <prastoin@e1r7p10.42.fr>
2019-01-28 18:21:04 +0100

    pas ouf

--- algo.c
+++ algo.c
@@ -6,11 +6,25 @@
 /*   By: prastoin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/28 13:41:56 by prastoin          #+#    #+#             */
-/*   Updated: 2019/01/28 16:53:48 by prastoin         ###   ########.fr       */
+/*   Updated: 2019/01/28 18:20:52 by prastoin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
+
+int		ft_restart(t_room *room, t_algo *algo)
+{
+//	if (room[algo->index_end].i < room[algo->index_end].links)
+//		return (-42);
+	printf ("RESTART%d\n", room[algo->index_end].i);
+	if (room[ft_atoi(room[algo->index_end].way[room[algo->index_end].i])].i < room[ft_atoi(room[algo->index_end].way[room[algo->index_end].i])].links)
+		return (algo->index_end);
+	else
+		room[algo->index_end].i++;
+	if (room[algo->index_end].i >= room[algo->index_end].links)
+		return (-42);
+	return (algo->index_end);
+}
 
 int		ft_previous(t_room *room, int index)
 {
@@ -33,17 +47,19 @@ int		ft_fill_power(t_room *room, t_algo *algo)
 	index = algo->index_end;
 	while (index != -42)
 	{
-		printf("index =%d\n", index);
-		if (ft_atoi(room[index].way[room[index].i]) != room[index].previous)
+		if (room[index].i < room[index].links && ft_atoi(room[index].way[room[index].i]) != room[index].previous && (room[ft_atoi(room[index].way[room[index].i])].start_end != 2))
 		{
 			next = ft_atoi(room[index].way[room[index].i]);
+			printf("From %d to %d\n", index, next);
 			room[next].previous = index;
 			if (room[index].power + 1 <= room[next].power)
 				room[next].power = room[index].power + 1;
-			room[index].i++;
+			if (room[index].start_end != 2)
+				room[index].i++;
 			index = next;
 			if (room[index].start_end == 1)
-				index = ft_previous(room, index);
+				if ((index = ft_restart(room, algo)) == -42)
+					break ;
 		}
 		else
 		{
