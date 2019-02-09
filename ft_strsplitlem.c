22f8461801e2be0b399553ebb98a982f91019453
Paul RASTOIN <prastoin@e2r8p1.42.fr>
2019-02-09 03:56:24 +0100

    ft_strsplit a la norm

--- ft_strsplitlem.c
+++ ft_strsplitlem.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strsplit.c                                      :+:      :+:    :+:   */
+/*   ft_strsplitlem.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: prastoin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/14 10:54:47 by prastoin          #+#    #+#             */
-/*   Updated: 2019/02/08 18:20:25 by prastoin         ###   ########.fr       */
+/*   Created: 2019/02/09 03:50:31 by prastoin          #+#    #+#             */
+/*   Updated: 2019/02/09 03:55:54 by prastoin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
-static int	ft_countwords(char *s, char c)
+static int		ft_countwords(char *s, char c)
 {
 	int	i;
 	int	count_words;
@@ -37,7 +37,7 @@ static int	ft_countwords(char *s, char c)
 	return (count_words);
 }
 
-static int	ft_len_words(char *s, int c, int k)
+static int		ft_len_words(char *s, int c, int k)
 {
 	int	len;
 
@@ -50,35 +50,44 @@ static int	ft_len_words(char *s, int c, int k)
 	return (len);
 }
 
-char		**ft_strsplit_lem(char const *s, char c)
+static int		ft_cut(char const *s, char c, char **tab, int nbr_words)
 {
-	int		i;
-	int		k;
-	int		nbr_words;
-	char	**tab;
-	int		len;
+	int	k;
+	int	i;
+	int	len;
 
-	i = 0;
 	k = 0;
-	if (!s)
-		return (NULL);
-	nbr_words = ft_countwords((char *)s, c);
-	if (!(tab = (char**)malloc(sizeof(char *) * (nbr_words + 1))))
-		return (NULL);
+	i = 0;
+	len = 0;
 	while (i < nbr_words)
 	{
 		if (s[k] == c && s[k] && k > 0)
 			k++;
-		if (!(tab[i] = ft_strsub_lem((char *)s, k, ft_len_words((char *)s, c, k))))
-			return (NULL);
+		if (!(tab[i] = ft_strsub_lem((char *)s, k,
+						ft_len_words((char *)s, c, k))))
+			return (-1);
 		if ((len = ft_len_words((char *)s, c, k)) == 0)
 		{
 			tab[i + 1] = NULL;
-			return (tab);
+			return (0);
 		}
 		i++;
 		k += ft_len_words((char *)s, c, k);
 	}
 	tab[i] = NULL;
+	return (0);
+}
+
+char			**ft_strsplit_lem(char const *s, char c)
+{
+	int		nbr_words;
+	char	**tab;
+
+	if (!s)
+		return (NULL);
+	nbr_words = ft_countwords((char *)s, c);
+	if (!(tab = (char**)malloc(sizeof(char *) * (nbr_words + 1))))
+		return (NULL);
+	ft_cut(s, c, tab, nbr_words);
 	return (tab);
 }
